8c28a7fd37b1d3e40a8ad46ca769e3372b9549b6
Zenasni <sirineboualizenasni@gmail.com>
2019-05-16 11:32:59 +0800

    create and store JS file in new folder

---
+++ js/main.js
@@ -0,0 +1,74 @@
+// initialize firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyAgzOd6iyHmM1au_nYBzbwcJ7EcwQAJa6g",
+    authDomain: "unit9-project.firebaseapp.com",
+    databaseURL: "https://unit9-project.firebaseio.com",
+    projectId: "unit9-project",
+    storageBucket: "unit9-project.appspot.com",
+    messagingSenderId: "1013251011486",
+    appId: "1:1013251011486:web:652948bad7c0d191"
+};
+  
+firebase.initializeApp(firebaseConfig);
+
+// connect to firebase application 
+var database = firebase.database();
+
+$("#reservation-form").on("submit", function(event) {
+    event.preventDefault();
+    
+    var userInput = {
+        name: $("#name").val(),
+        day: $("#day").val()
+    };
+
+    $("#name").val("");
+    $("form #day").val("");
+    console.log("userInput:", userInput);
+    console.log("userInput.name:", userInput.name);
+    console.log("userInput.day:", userInput.day);
+    var reservationDetails = database.ref("details");
+    reservationDetails.push(userInput);
+});
+
+var query = firebase.database().ref("details").orderByKey();
+query.once("value")
+.then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+    // key will be "ada" the first time and "alan" the second time
+    var key = childSnapshot.key;
+    // childData will be the actual contents of the child
+    var childData = childSnapshot.val();
+    console.log("key: ", key);
+    console.log("childData: ", childData);
+
+    });
+});
+
+// retreive data
+//function getDetails () {
+    //database.ref("details").on("value", function(results) {
+        //var allDetails = results.val();
+        //console.log("allDetails: ", allDetails);
+
+        //var detail = [];
+
+        //for (var item in allDetails) {
+        //    var context = {
+        //        name: allDetails[item].name,
+        //        day: allDetails[item].day
+        //    };
+
+        //    var source = $("#template").html();
+
+        //    var template = Handlebars.compile(source);
+
+        //    var reservationListDetails = template(context);
+
+        //    detail.push(reservationListDetails);
+        //}
+        //$(".details").empty();
+    //});
+//}
+
+//getDetails();
