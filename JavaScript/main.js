d5e23b87092a751a1b5a8ef90a308db9ce31e838
Zenasni <sirineboualizenasni@gmail.com>
2019-05-17 11:04:01 +0800

    adding map with Google Map API

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -101,3 +101,16 @@ query.once("value")
 }*/
 
 getReservation();
+
+//add google map
+// Initialize and add the map
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        center: {lat: 40.8054491, lng: -73.9654415},
+        zoom: 10
+    });
+    var marker = new google.maps.Marker({
+        position: {lat: 40.8054491, lng: -73.9654415},
+        map: map
+    });
+}
